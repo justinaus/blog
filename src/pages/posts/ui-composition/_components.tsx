4f0a1369d043e97a52c73332895899567607bc14
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2024-06-11 17:10:20 +0900

    Update markdown styles

---
+++ src/pages/posts/ui-composition/_components.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export function Card({ children }: { children: React.ReactNode }) {
+  return (
+    <Card.Wrap>
+      <Card.Section>{children}</Card.Section>
+    </Card.Wrap>
+  );
+}
+
+Card.Wrap = function Wrap({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      style={{
+        borderRadius: '4px',
+        border: '2px solid lightgray',
+        boxShadow:
+          'rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgb(51, 161, 204) 8px 8px 0px 0px',
+        maxWidth: '50ch',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+Card.Section = function Section({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      style={{
+        padding: '1rem',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+Card.Divider = function Divider() {
+  return <hr className="h-[2px] w-full bg-gray-300" />;
+};
