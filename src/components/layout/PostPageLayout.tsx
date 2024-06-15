ece4ecc2fff5ffafa5d0a9d5d1fb7bcbbee14d6e
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2024-06-16 01:32:14 +0900

    Update post page layout

--- src/components/layout/PostPageLayout.tsx
+++ src/components/layout/PostPageLayout.tsx
@@ -1,5 +1,4 @@
-import { AppShell, Burger, Stack, Text } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Stack, Text } from '@mantine/core';
 import { format } from 'date-fns';
 import { useMemo } from 'react';
 
@@ -11,7 +10,7 @@ export default function PostPageLayout({
   date,
   children,
 }: React.PropsWithChildren<Props>) {
-  const [opened, { toggle }] = useDisclosure();
+  // const [opened, { toggle }] = useDisclosure();
 
   const dateText = useMemo(() => {
     return format(new Date(date), 'MMMM dd, yyyy');
@@ -20,18 +19,29 @@ export default function PostPageLayout({
   return (
     <AppShell
       header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: 'sm',
-        collapsed: { mobile: !opened },
+      // navbar={{
+      //   width: 300,
+      //   breakpoint: 'sm',
+      //   collapsed: { mobile: !opened },
+      // }}
+      // padding="md"
+      maw={'50rem'}
+      style={{
+        margin: '0 auto',
       }}
-      padding="md"
     >
-      <AppShell.Header>
-        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+      <AppShell.Header
+        px={'xl'}
+        maw={'50rem'}
+        bd={'none'}
+        style={{
+          margin: '0 auto',
+        }}
+      >
+        {/* <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" /> */}
         <div>Logo</div>
       </AppShell.Header>
-      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+      {/* <AppShell.Navbar p="md">Navbar</AppShell.Navbar> */}
       <AppShell.Main>
         <Stack gap="xl" p={'xl'}>
           <Text>{dateText}</Text>
